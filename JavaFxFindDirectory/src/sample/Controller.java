5f6514daa6ae7d3c468ad9d35f13368a90969a4f
iraywave <202568@edu.fa.ru>
2023-09-09 11:21:55 +0300

    FindDirectoryExam

---
+++ JavaFxFindDirectory/src/sample/Controller.java
@@ -0,0 +1,47 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextArea;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * Данный класс создан для работы с javaFx
+ * @author Шинкевич АО ПИ20-1в
+ */
+public class Controller {
+    @FXML
+    TextArea path_fld;
+    @FXML
+    TextArea result_fld;
+
+    /**
+     * Данный метод обрабатывает нажатие на кнопку, отдает во view пользователя список файлов, лежащих в указанной пользователем директории
+     */
+    @FXML
+    public void clickBtn(){
+        result_fld.clear();
+        File file = new File(path_fld.getText()); // path_fld - текстовое поле, где пользователь задает путь
+        if (file.isDirectory()){ // если файл является директорией
+            ArrayList<String> arrayList = new ArrayList<>();
+            for (String i : file.list()){
+                //arrayList.add(file.getAbsolutePath() + File.separator + i + '\n');
+                arrayList.add(file.getAbsolutePath() + File.separator + i + '\n');
+            }
+            if (arrayList.size() > 0){
+                result_fld.setText(arrayList.toString());
+            }
+            else {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Попробуйте позже");
+                alert.showAndWait();
+            }
+        }
+        else {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Вы допустили ошибку");
+            alert.showAndWait();
+        }
+        path_fld.clear();
+    }
+}
